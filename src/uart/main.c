08fb948ade05c548159fdc32712f132e1891d032
Nick Sinas <nicksinas@gmail.com>
2021-09-22 18:11:21 -0500

    Initial upload

---
+++ src/uart/main.c
@@ -0,0 +1,206 @@
+/**
+ * Copyright (c) 2014 - 2020, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+ * @defgroup uart_example_main main.c
+ * @{
+ * @ingroup uart_example
+ * @brief UART Example Application main file.
+ *
+ * This file contains the source code for a sample application using UART.
+ *
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "app_uart.h"
+#include "app_error.h"
+#include "nrf_delay.h"
+#include "nrf.h"
+#include "bsp.h"
+#if defined (UART_PRESENT)
+#include "nrf_uart.h"
+#endif
+#if defined (UARTE_PRESENT)
+#include "nrf_uarte.h"
+#endif
+
+#include "cli.h"
+
+#define UART_TX_BUF_SIZE 256                         /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE 256                         /**< UART RX buffer size. */
+
+static cli_status_t help_func(int argc, char **argv);
+static cli_status_t ble_func(int argc, char **argv);
+static cli_status_t gpio_func(int argc, char **argv);
+static cli_status_t adc_func(int argc, char **argv);
+
+cmd_t cmd_tbl[4] = {
+    {
+        .cmd = "help",
+        .func = help_func
+    },
+    {
+        .cmd = "ble",
+        .func = ble_func
+    },
+    {
+        .cmd = "gpio",
+        .func = gpio_func
+    },
+    {
+        .cmd = "adc",
+        .func = adc_func
+    }
+};
+
+cli_t cli;
+cli_status_t rslt = CLI_OK;
+
+void user_uart_println(char *string)
+{
+    printf(string);
+}
+
+cli_status_t help_func(int argc, char **argv)
+{
+    cli_status_t ok = CLI_OK;
+    cli.println("HELP function executed\n");
+    return ok;
+}
+
+cli_status_t ble_func(int argc, char **argv)
+{
+    cli_status_t ok = CLI_OK;
+    if(argc > 1)
+    {
+        if(strcmp(argv[1], "-help") == 0)
+        {
+            cli.println("BLINK help menu\n");
+        }
+    }
+    else
+    {
+        cli.println("BLINK function executed\n");
+    }
+    return ok;
+}
+
+cli_status_t gpio_func(int argc, char **argv)
+{
+    cli_status_t ok = CLI_OK;
+    cli.println("GPIO function executed\n");
+    return ok;
+}
+
+cli_status_t adc_func(int argc, char **argv)
+{
+    cli_status_t ok = CLI_OK;
+    cli.println("ADC function executed\n");
+    return ok;
+}
+
+void uart_error_handle(app_uart_evt_t * p_event)
+{
+    if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR)
+    {
+        APP_ERROR_HANDLER(p_event->data.error_communication);
+    }
+    else if (p_event->evt_type == APP_UART_FIFO_ERROR)
+    {
+        APP_ERROR_HANDLER(p_event->data.error_code);
+    }
+    else if (p_event->evt_type == APP_UART_DATA_READY)
+    {
+        uint8_t cr;
+        while (app_uart_get(&cr) != NRF_SUCCESS);
+        cli_put(&cli, cr);
+    }
+}
+
+
+/**
+ * @brief Function for main application entry.
+ */
+int main(void)
+{
+    uint32_t err_code;
+
+    bsp_board_init(BSP_INIT_LEDS);
+
+    const app_uart_comm_params_t comm_params =
+      {
+          RX_PIN_NUMBER,
+          TX_PIN_NUMBER,
+          RTS_PIN_NUMBER,
+          CTS_PIN_NUMBER,
+          APP_UART_FLOW_CONTROL_DISABLED,
+          false,
+          NRF_UART_BAUDRATE_115200
+      };
+
+    APP_UART_FIFO_INIT(&comm_params,
+                         UART_RX_BUF_SIZE,
+                         UART_TX_BUF_SIZE,
+                         uart_error_handle,
+                         APP_IRQ_PRIORITY_LOWEST,
+                         err_code);
+
+    APP_ERROR_CHECK(err_code);
+
+    cli.println = user_uart_println;
+    cli.cmd_tbl = cmd_tbl;
+    cli.cmd_cnt = sizeof(cmd_tbl)/sizeof(cmd_t);
+    if((rslt = cli_init(&cli)) != CLI_OK)
+    {
+        printf("CLI: Failed to initialise");
+    }
+
+    while(1)
+    {
+        cli_process(&cli);
+    }
+
+    return 0;
+}
+
+
+/** @} */
